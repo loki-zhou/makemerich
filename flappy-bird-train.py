93d50ca949c1aaa05299a09605e37e175944c202
zhouyi <zhouy@zhiyun-tech.com>
2024-05-24 12:01:45 +0800

    add some learning demo

--- flappy-bird-train.py
+++ flappy-bird-train.py
@@ -6,6 +6,9 @@ import os
 from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.vec_env import VecFrameStack
+
+
 
 # 创建单个环境的工厂函数
 def make_env():
@@ -20,6 +23,8 @@ num_envs = 8
 envs = gym.make("FlappyBird-v0",use_lidar=False)
 
 # envs = DummyVecEnv([make_env() for _ in range(num_envs)])
+env = DummyVecEnv([lambda: envs])
+env = VecFrameStack(env,4,channels_order='last')
 
 monitor_dir = "./model_dir/FlappyBird/"
 os.makedirs(monitor_dir, exist_ok=True)
@@ -35,14 +40,29 @@ import torch as th
 policy_kwargs = dict(
                      net_arch=dict(pi=[256, 256], vf=[256, 256]))
 
-model = PPO("MlpPolicy", envs, policy_kwargs=policy_kwargs, tensorboard_log="./tlog/FlappyBird/ppo/",
+model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, tensorboard_log="./tlog/FlappyBird/ppo/",
             batch_size=256,
-            gamma=0,
+            gamma=0.9,
             verbose=1)
 
 # rl_model_3235000_steps.zip
 # model.set_parameters("model_dir/FlappyBird_best.zip")
 
-model.learn(total_timesteps=3_000_000, callback=checkpoint_callback)
+model.learn(total_timesteps=1000_000, callback=checkpoint_callback)
 
-model.save("model_dir/FlappyBird_bestV2.zip")
+# model.learn(total_timesteps=10_000, callback=checkpoint_callback)
+
+# model.set_env(env)
+model.save("model_dir/FlappyBird_bestV3.zip")
+
+
+
+total_reward = 0
+done, truncated = False, False
+observation= env.reset()
+while not done and not truncated:
+    action, _states = model.predict(observation, deterministic=True)
+    observation, reward, done, info = env.step(action)
+    total_reward = total_reward + reward
+    env.render()
+print("total_reward = ", total_reward)
--- flappy-bird-use_model.py
+++ flappy-bird-use_model.py
@@ -5,13 +5,20 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import gymnasium as gym
 from stable_baselines3 import PPO
-
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.vec_env import VecFrameStack
 
 env = gym.make("FlappyBird-v0", render_mode="human", use_lidar=False)
 # env = gym.make("FlappyBird-v0",  use_lidar=False)
 # env = gym.wrappers.NormalizeObservation(env)
+
+# envs = gym.make("FlappyBird-v0",use_lidar=False)
+
+# envs = DummyVecEnv([make_env() for _ in range(num_envs)])
+env = DummyVecEnv([lambda: env])
+env = VecFrameStack(env,4,channels_order='last')
 #
-model = PPO.load("model_dir/FlappyBird_bestV2.zip")
+model = PPO.load("model_dir/FlappyBird_bestV3.zip")
 # model = PPO.load("model_dir/FlappyBird_best.zip")
 # model = PPO.load("model_dir/FlappyBird/rl_model_3000000_steps.zip")
 
@@ -34,10 +41,10 @@ reward_list = []
 
 total_reward = 0
 done, truncated = False, False
-observation, info = env.reset()
+observation= env.reset()
 while not done and not truncated:
     action, _states = model.predict(observation, deterministic=True)
-    observation, reward, done, truncated, info = env.step(action)
+    observation, reward, done,  info = env.step(action)
     total_reward = total_reward + reward
     env.render()
 print("total_reward = ", total_reward)
