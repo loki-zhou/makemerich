3e88cab957e4864ce021769d9fd2abc2e6d81428
loki-zhou <loki_zhou@hotmail.com>
2024-08-09 09:52:22 +0800

    turning some struct

--- nlp/ch02/count_method_small.py
+++ nlp/ch02/count_method_small.py
@@ -19,9 +19,14 @@ np.set_printoptions(precision=3)  # 有效位数为3位
 print(C[0])
 print(W[0])
 print(U[0])
+print("*"*30)
+print(U)
+print("*"*30)
 
 # plot
 for word, word_id in word_to_id.items():
     plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
+print(U[:,0])
+print(U[:,1])
 plt.scatter(U[:,0], U[:,1], alpha=0.5)
 plt.show()
