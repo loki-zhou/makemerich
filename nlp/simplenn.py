c3a440fb71a5df467749a4d66ef5532029152211
loki-zhou <loki_zhou@hotmail.com>
2024-08-15 23:13:04 +0800

    node

--- nlp/simplenn.py
+++ nlp/simplenn.py
@@ -1,12 +1,51 @@
+import numpy as np
 
-X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
-y = np.array([[0,1,1,0]]).T
-syn0 = 2*np.random.random((3,4)) - 1
-syn1 = 2*np.random.random((4,1)) - 1
-for j in xrange(60000):
-    l1 = 1/(1+np.exp(-(np.dot(X,syn0))))
-    l2 = 1/(1+np.exp(-(np.dot(l1,syn1))))
-    l2_delta = (y - l2)*(l2*(1-l2))
-    l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += X.T.dot(l1_delta)
+
+# compute sigmoid nonlinearity
+def sigmoid(x):
+    output = 1 / (1 + np.exp(-x))
+    return output
+
+
+# convert output of sigmoid function to its derivative
+def sigmoid_output_to_derivative(output):
+    return output * (1 - output)
+
+
+# input dataset
+X = np.array([[0, 1],
+              [0, 1],
+              [1, 0],
+              [1, 0]])
+
+# output dataset
+y = np.array([[0, 0, 1, 1]]).T
+
+# seed random numbers to make calculation
+# deterministic (just a good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+synapse_0 = 2 * np.random.random((2, 1)) - 1
+
+for iter in range(10000):
+    # forward propagation
+    layer_0 = X
+    layer_1 = sigmoid(np.dot(layer_0, synapse_0))
+
+    # how much did we miss?
+    layer_1_error = layer_1 - y
+
+    # multiply how much we missed by the
+    # slope of the sigmoid at the values in l1
+    layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
+    synapse_0_derivative = np.dot(layer_0.T, layer_1_delta)
+
+    # update weights
+    synapse_0 -= synapse_0_derivative
+
+print("Output After Training:")
+
+print(layer_1)
+
+
